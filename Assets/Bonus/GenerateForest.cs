6913dda61808af0e8b4af1a3f442a625cbbc291e
RiccardoUnity <beninriccardo@gmail.com>
2025-04-21 11:56:22 +0200

    Hotfix
    Aggiunto limite temporale alla quantità di calcoli per la generazione della foresta nella scena Bonus
    Ora non può più avviare un loop infinito

--- Assets/Bonus/GenerateForest.cs
+++ Assets/Bonus/GenerateForest.cs
@@ -1,82 +1,75 @@
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
 using UnityEngine;
+using Debug = UnityEngine.Debug;
 
 public class GenerateForest : MonoBehaviour
 {
     [Tooltip("Rende visibile i log di debug")]
     public bool debug = true;
 
+    [Tooltip("Materiali possibili per la chioma")]
     public Material[] materials = new Material[5];
+    [Tooltip("Possibili asset da istanziare, ATTENZIONE devono avere un oggetto figlio Chioma")]
     public GameObject[] albero = new GameObject[4];
-    [Range(5, 20)]
-    public int numeroAlberi = 10;
-    [Tooltip("Raggio della foresta da generare, cambiare con cautela")]
-    public float raggioForesta = 5f;
+    [Tooltip("Quantità massima di alberi da generare")]
+    [Range(5, 40)]
+    public int numeroAlberi = 15;
+    [Tooltip("Lato della foresta da generare")]
+    [Range(5, 10)]
+    public float latoForesta = 10f;
+    private int latoForestaInt = 0;
+    public Transform centroForesta;
+    private Vector3 deltaCentroForesta = Vector3.zero;   //Serve per centrare la posizione durante il calcolo random
     [Tooltip("Il nome della chioma dell'albero deve essere lo stesso per ogni GameObject inserito nell'array albero")]
     public string nomeChioma = "Chioma";
     List<GameObject> alberiForesta = new List<GameObject>();
     List<Vector3> posizioniAlberi = new List<Vector3>();
+    
+    private System.Random random = new System.Random();   //Generare numeri random in un altro Thread
+    private int risoluzioneFloat = 1000;   //Per non dover usare double
+    private int contoCalcoli = 0;   //Per non dover usare return
 
     //Potevo usare gli eventi ... ma per provarlo ho preferito fare così
-    [Tooltip("Impostare VERO per ricalcolare lo script")]
-    public bool rigera = false;
+    [Tooltip("Impostare VERO per ricalcolare lo script in RunTime")]
+    public bool rigenera = false;
 
-    private void Forest()
-    {
-        GameObject nuovoAlbero;
-        MeshRenderer chiomaMR;
-        
-        int randomTipoAlbero = 0;
-        int randomMaterial = 0;
-
-        //Resetto le liste
-        alberiForesta.Clear();
-        posizioniAlberi.Clear();
-
-        //Genero la posizione degli alberi
-        PosizioniValideAlberi();
-
-        //Genero la foresta
-        for (int i = 0; i < numeroAlberi; i++)
-        {
-            //Scelgo una variante dell'albero e lo istanzio
-            randomTipoAlbero = Random.Range(0, albero.Length);
-            nuovoAlbero = Instantiate(albero[randomTipoAlbero], posizioniAlberi[i], Quaternion.identity);
-            alberiForesta.Add(nuovoAlbero);
-
-            //Assegno un nuovo material alla chioma dell'albero appena istanziato
-            chiomaMR = nuovoAlbero.transform.Find(nomeChioma).GetComponent<MeshRenderer>();
-            randomMaterial = Random.Range(0, materials.Length);
-            chiomaMR.material = materials[randomMaterial];
-        }
-    }
-
+    //ATTENZIONE: BACKGROUND - THREAD SECONDARIO
     //Genero una posizione random per un albero
     private Vector3 PosizioneAlbero()
     {
-        Vector2 posizioneRandom = Vector2.zero;
-        float moltiplicatore = 0f;
-
-        posizioneRandom = Random.insideUnitSphere;
-        moltiplicatore = Random.Range(0f, raggioForesta);
-        posizioneRandom *= moltiplicatore;
-        return new Vector3(posizioneRandom.x, 0f, posizioneRandom.y);
+        return new Vector3((random.Next(0, latoForestaInt)) / risoluzioneFloat,
+                            0f,
+                            (random.Next(0, latoForestaInt)) / risoluzioneFloat)
+            + deltaCentroForesta;
     }
 
-    //Genero tutte le posizioni per gli alberi verificandone la distanza
-    private void PosizioniValideAlberi()
+    /*ATTENZIONE: BACKGROUND - THREAD SECONDARIO
+    Uso un thread secondario per interrompere il ciclo while così da usare tutta la potenza disponibile
+    nell'arco di un secondo, tempo in cui il giocatore, in questo caso, è disposto ad aspettare
+
+    Genero tutte le posizioni per gli alberi verificandone la distanza */
+    private void PosizioniValideAlberi(float secondiMassimiCalcoli)
     {
         Vector3 possibilenuovaPosizione = Vector3.zero;
         bool cercaNuovaPosizione = true;
         bool troppoVicino = false;
+        //Resetto il conto dei calcoli eseguiti
+        contoCalcoli = 0;
 
+        //Avvio il conto del tempo del sistema operativo
+        Stopwatch tempo = Stopwatch.StartNew();
+
+        //Inserisco la prima posizione del primo albero sapendo che sarà sicuramente valida e fornisce un mezzo di paragone per i prossimi calcoli
         posizioniAlberi.Add(PosizioneAlbero());
-        for (int i = 1; i <= numeroAlberi; i++)
+        for (int i = 1; i < numeroAlberi; i++)
         {
             cercaNuovaPosizione = true;
-            //Continuo a cercare una posizione valida finchè non la trovo
-            while (cercaNuovaPosizione)
+            //Continuo a cercare una posizione valida finchè non la trovo o il tempo finisce
+            while (cercaNuovaPosizione && tempo.Elapsed.TotalSeconds < secondiMassimiCalcoli)
             {
+                contoCalcoli++;
                 possibilenuovaPosizione = PosizioneAlbero();
                 //Confronto ogni posizione registrata con la possibile nuova posizione
                 foreach (Vector3 posizione in posizioniAlberi)
@@ -84,40 +77,80 @@ public class GenerateForest : MonoBehaviour
                     if (Vector3.Distance(possibilenuovaPosizione, posizione) < 1.25f)   //Usata distanza arbitraria sapendo la grandezza del prefab
                     {
                         troppoVicino = true;
-                        if (debug)
-                            Debug.Log("Posizione troppo vicina");
                     }
                 }
                 //Verifico il risultato del confronto
                 if (!troppoVicino)
                 {
-                    cercaNuovaPosizione = false;
+                    cercaNuovaPosizione = false;   //Esco dal ciclo while per calcolare la prossima posizione
                     //Registro la nuova posizione
                     posizioniAlberi.Add(possibilenuovaPosizione);
-                    Debug.Log("Posizione albero valida");
                 }
                 troppoVicino = false;
             }
         }
     }
 
+    //Funzione principale per generare una foresta
+    //ESEGUITA nel MAIN-THREAD in modo asincrono per aspettare l'esecuzione in background delle posizioni degli alberi
+    //Unity continuerà a fare le sue cose mentre si aspetta, il giocatore potrebbe per esempio continuare a muoversi
+    async void Forest()
+    {
+        GameObject nuovoAlbero;
+        MeshRenderer chiomaMR;
+
+        int randomTipoAlbero = 0;
+        int randomMaterial = 0;
+
+        //Resetto parametro per rigenerare la foresta nell'Update
+        rigenera = false;
+        latoForestaInt = (int)(risoluzioneFloat * latoForesta);   //Uso variabile int per non dover usare double in background ...
+        deltaCentroForesta = centroForesta.position - new Vector3(latoForesta / 2f, 0f, latoForesta / 2f);   //Differenza per centrare l'origine della foresta, usata in background
+
+        //Resetto le liste
+        alberiForesta.Clear();
+        posizioniAlberi.Clear();
+
+        //Genero la posizione degli alberi in un Thread secondario per non bloccare Unity
+        await Task.Run(() => PosizioniValideAlberi(1f));
+        if (debug)
+        {
+            Debug.Log("Calcoli eseguiti: " + contoCalcoli);
+            Debug.Log("Alberi generati: " + posizioniAlberi.Count);
+        }
+
+        //Genero la foresta
+        foreach (Vector3 posAlb in posizioniAlberi)
+        {
+            //Scelgo una variante dell'albero e lo istanzio
+            randomTipoAlbero = Random.Range(0, albero.Length);
+            nuovoAlbero = Instantiate(albero[randomTipoAlbero], posAlb, Quaternion.identity);
+            alberiForesta.Add(nuovoAlbero);
+
+            //Assegno un nuovo material alla chioma dell'albero appena istanziato
+            //I prefab sono tali solo nell'editor e negli assets, non in runtime
+            chiomaMR = nuovoAlbero.transform.Find(nomeChioma).GetComponent<MeshRenderer>();
+            randomMaterial = Random.Range(0, materials.Length);
+            chiomaMR.material = materials[randomMaterial];
+        }
+    }
+
     void Start()
     {
-        Debug.Log("ATTENZIONE: lo script GenerateForest ha una criticità," +
-            " non verifica se per la quantità di alberi inserita l'area di istanziamento è abbastanza grande, potrebbe andare in loop con parametri diversi da questi");
         Forest();
     }
 
     void Update()
     {
-        if (rigera)
+        if (rigenera)
         {
+            //Cancello la foresta esistente
             foreach(GameObject albero in alberiForesta)
             {
                 Destroy(albero);
             }
+            //Genero una foresta nuova
             Forest();
-            rigera = false;
         }
     }
 }
